23f61d319487ecc236a62db63acddfa87e1a0e4e
Wapkagut <ulrich.damien@gmail.com>
2020-12-10 16:47:18 +0100

    Réparation du login

--- modules/user/controllers/authentification.js
+++ modules/user/controllers/authentification.js
@@ -1,33 +1,37 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
 module.exports = {
     login,
     logout
 }
 
-function login(req,res,next){
+function login(req,res,next) {
     const email = req.body.email;
-    const password = req.body.password;
-    const sqlQuery = 'SELECT * FROM `user` WHERE e_mail = ? AND mot_de_passe = ?'
-    db.query(sqlQuery,[email, password], (err,result) => {
-        if(err || !result || !result[0]) {
-            console.log(err);
-            console.log(result);
-            console.log(result[0]);
-
+    const password = req.body.password
+    const sqlQuery = 'SELECT * FROM `user` WHERE e_mail = ?'
+    db.query(sqlQuery, [email], (err, result) => {
+        if (err || !result || !result[0]) {
             res.status(401).json({message: 'utilisateur introuvable'});
             return;
         }
-        console.log("env value")
-        console.log(process.env.SECRET_TOKEN_GENERATOR);
-        const token = jwt.sign(
-            {userId: result[0].id_user},
-            process.env.SECRET_TOKEN_GENERATOR,
-            { expiresIn:'7d'}
-        );
-        res.status(200).json({
-            userId:result[0].id_user,
-            token: token
-        });
+        console.log("pass: ", result[0].mot_de_passe)
+        bcrypt.compare(password, result[0].mot_de_passe, (err, result2) => {
+            console.log("reponse : ", result2)
+            if(result2) {
+                const token = jwt.sign(
+                    {userId: result[0].id_user},
+                    process.env.SECRET_TOKEN_GENERATOR,
+                    {expiresIn: '7d'}
+                );
+                res.status(200).json({
+                    userId: result[0].id_user,
+                    token: token
+                });
+            }
+            else{
+                res.status(401).json({message: 'utilisateur introuvable'});
+            }
+        })
     })
 }
 
