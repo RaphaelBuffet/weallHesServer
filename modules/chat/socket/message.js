2806707bb65a1d85c9ce759f37f4a2a01f35946b
Wapkagut <ulrich.damien@gmail.com>
2020-12-15 23:10:26 +0100

    Fix sending and receiving msg

--- modules/chat/socket/message.js
+++ modules/chat/socket/message.js
@@ -10,17 +10,18 @@ let Message = class {
     static postMessage(msg){
         msg = JSON.parse(msg);
         console.log(msg);
-        io.emit(msg.to.id, JSON.stringify(msg));
-        io.emit(msg.from.id, JSON.stringify(msg));
+        console.log(msg);
+        io.emit(msg.idUser1, JSON.stringify(msg));
+        io.emit(msg.idUser2, JSON.stringify(msg));
         this.recordMsg(msg);
     }
 
     static recordMsg(msg){
-        if(typeof msg.from.id !== 'number' || typeof msg.to.id !== 'number'){
+        if(typeof msg.idUser1 !== 'number' || typeof msg.idUser2 !== 'number'){
             return;
         }
         console.log(msg);
-        db.query('INSERT INTO log_chat(id_user1, id_user2,message) VALUE (?,?,?)', [msg.from.id,msg.to.id,msg.msg], (err, res) => {
+        db.query('INSERT INTO log_chat(id_user1, id_user2,message) VALUE (?,?,?)', [msg.idUser1,msg.idUser2,msg.msg], (err, res) => {
             if(err) console.log(err);
         })
     }
