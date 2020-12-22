9c63092cf31911bd3eb01043677e33a56029bf9a
Wapkagut <ulrich.damien@gmail.com>
2020-12-22 14:56:55 +0100

    Fix sending and receiving file

--- files/temp.js
+++ files/temp.js
@@ -1,67 +1,7 @@
-let db, config;
-module.exports= TempFiles = class {
+module.exports = TempFiles = class {
     static sendTempsFiles(myId, otherId, file){
         return new Promise((next)=>{
             console.log(file)
-        })
-    }
-
-    static myHistory(id){
-        return new Promise((next) => {
-            db.query('SELECT * FROM chatLogView WHERE id_user1 = ? OR id_user2 = ?', [id,id], (err, rows) => {
-                if(err){console.log(err);}
-                else {
-                    let result = {};
-                    for (const value of rows){
-                        let idOther = value.id_user1 === Number(id) ? value.id_user2 : value.id_user1;
-                        if(typeof result[idOther] === 'undefined'){
-                            result[idOther] = {};
-                            result[idOther].msg = [];
-                            result[idOther].id = idOther;
-                            result[idOther].name = value.id_entreprise === idOther ? value.nom_entreprise : value.nom_postulant;
-                        }
-                        result[idOther].msg.push({
-                            idUser1 : value.id_user1,
-                            idUser2 : value.id_user2,
-                            msg : value.message,
-                            date : value.date
-                        });
-                    }
-                    next(result);
-                }
-            })
-        })
-    }
-
-    static myHistoryWith(myId, otherId){
-        return new Promise((next) => {
-            db.query('SELECT * FROM chatLogView WHERE (id_user1 = ? AND id_user2 = ?) OR (id_user1 = ? AND id_user2 = ?)', [myId, otherId, otherId, myId], (err, rows) =>{
-                if(err){console.log(err);}
-                else {
-                    myId = Number(myId);
-                    otherId = Number(otherId);
-                    let result = {};
-                    result.msg = [];
-                    if(rows.length > 0)
-                        result.correspondent = {
-                            id:otherId,
-                            name: rows[0].id_entreprise === otherId ? rows[0].nom_entreprise : rows[0].nom_postulant
-                        }
-                    else result.correspondent = {
-                        id:otherId,
-                        name:'unknown'
-                    }
-                    for(const value of rows){
-                        result.msg.push({
-                            idUser1 : value.id_user1,
-                            idUser2 : value.id_user2,
-                            msg : value.message,
-                            date : value.date
-                        })
-                    }
-                    next(result);
-                }
-            })
         })
     }
 }
--- middleware/Auth.js
+++ middleware/Auth.js
@@ -1,13 +1,18 @@
 const jwt = require('jsonwebtoken');
 
 function Auth(req,res,next){
-    const token = req.headers['authorization'].split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN_GENERATOR);
-    req.userId = decodedToken.userId;
-    if (req.body.userId && req.body.userId !== userId) {
-        res.status(400).json({message : 'token invalide'})
-    } else {
-        next();
+    console.log(req.headers['authorization']);
+    if(!req.headers['authorization'])
+        res.status(400).json({message : 'token invalide'});
+    else{
+        const token = req.headers['authorization'].split(' ')[1];
+        const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN_GENERATOR);
+        req.userId = decodedToken.userId;
+        if (req.body.userId && req.body.userId !== userId) {
+            res.status(400).json({message : 'token invalide'})
+        } else {
+            next();
+        }
     }
 }
 
